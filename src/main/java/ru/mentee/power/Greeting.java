9f7cb0842c2f1d90bbe180883a2e782969f18000
George Tukov <georgetukov21@gmail.com>
2025-08-24 20:30:14 +0300

    Добавление greeting

---
+++ src/main/java/ru/mentee/power/Greeting.java
@@ -0,0 +1,9 @@
+package ru.mentee.power;
+public class Greeting {
+    public static String getGreeting(Stirng name) {
+        return "Привет, " + name + "!";
+    }
+    public static void main(String[] args) {
+        System.out.println(getGreeting("Java разработчик"));
+    }
+}
